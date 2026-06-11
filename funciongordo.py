9d2e43b244d59497d452ac700c5386b5521188f1
Martin01011 <104696570+Martin01011@users.noreply.github.com>
2026-06-11 19:16:26 -0300

    funcion baja de vuelos martin

--- funciongordo.py
+++ funciongordo.py
@@ -15,38 +15,44 @@ def baja_vuelos(matriz_vuelos):
         codigo = input("Ingrese el codigo de vuelo que desea eliminar: ")
 
         for i in range(len(matriz)):
-            
+                
             if matriz[i][0] == codigo: #nos fijamos si esta el codigo
                 print("el vuelo encontrado tiene los datos {}".format(matriz[i]))
                 print()
                 vuelo_encontrado = 1 #para saber si encontro el vuelo para un if afeura del for
 
-                while eliminar == 0:
-                    
-                    eliminar = input("desea eliminarlo? (si/no) ")
-                    eliminar = eliminar.lower() #hacemos que la variable este si o si en minuscula
-                    
-                    if eliminar == "si":
-                        matriz.pop(i) #eliminamos el dato de la matriz de un valor en especifico
-                        print()
-                        print("Vuelo eliminado")
-                    elif eliminar == "no":
-                        print()
-                        print("eliminacion cancelada")
-                    else:
-                        print()
-                        print("invalido, intente nuevamente")
-                        eliminar = 0 #hace que el ciclo se siga cumpliendo para que ingrese un valor valido
-
+                if "Cancelado" in matriz[i]: #nos aseguramos que el estado operativo del vuelo sea cancelado
+                    while eliminar == 0:
+                
+                        eliminar = input("desea eliminarlo? (si/no) ")
+                        eliminar = eliminar.lower() #hacemos que la variable este si o si en minuscula
+                
+                        if eliminar == "si":
+                            matriz.pop(i) #eliminamos el dato de la matriz de un valor en especifico
+                            print()
+                            print("Vuelo eliminado")
+                        elif eliminar == "no":
+                            print()
+                            print("eliminacion cancelada")
+                        else:
+                            print()
+                            print("invalido, intente nuevamente")
+                            eliminar = 0 #hace que el ciclo se siga cumpliendo para que ingrese un valor valido
+                else:
+                    print("el estado operativo no es cancelado por eso no se puede dar de baja ese vuelo")
+        
         if vuelo_encontrado == 0:
+            print()
             print("no se encontro el vuelo")
 
         print()
-        otro_vuelo = int(input("desea buscar otro vuelo? si/no (1/2)"))
-        if otro_vuelo == 2:
+        otro_vuelo = input("desea buscar otro vuelo? si/no ")
+        otro_vuelo = otro_vuelo.lower()
+
+        if otro_vuelo == "no":
             salida = 2 #rompe el while
             print()
     
-    print("Fin de Baja de vuelos")
+    print((4*"-")+"Fin de Baja de vuelos"+(4*"-"))
     
     return matriz
